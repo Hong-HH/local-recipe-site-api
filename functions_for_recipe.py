6261cf00ddb312442fd01002ef2a061b1fc63123
Hong-HH <a1guskn7@gmail.com>
2022-11-14 13:55:55 +0900

    recipe_detail

--- functions_for_recipe.py
+++ functions_for_recipe.py
@@ -1,6 +1,9 @@
 
 # recipe list request type 에 대한 query 문 매칭
 
+
+
+
 def recipe_list_map (list_type) :
 
 
@@ -31,3 +34,53 @@ def recipe_list_map (list_type) :
                         }
 
     return list_type_map[list_type]
+
+
+
+recipe_detail_query = {
+                        "recipe_user_info" : ''' select r.id, r.user_id, u.nickname, u.profile_img, r.header_title as title, r.header_img as mainSrc, r.header_desc as intro, 
+                        r.created_at, r.updated_at,  c1.name as c_type, c2.name as c_ctx , c3.name as c_ind, c4.name as c_s, c5.name as c_time,c6.name as c_level
+                        from 
+                        (select *
+                        from recipe
+                        where id = %s ) as r
+                        left join
+                        category as c1
+                        on r.category_type = c1.id
+                        left join
+                        category as c2
+                        on r.category_context = c2.id
+                        left join
+                        category as c3
+                        on r.category_ingredients = c3.id
+                        left join
+                        category as c4
+                        on r.servings = c4.id
+                        left join
+                        category as c5
+                        on r.time = c5.id
+                        left join
+                        category as c6
+                        on r.level = c6.id 
+                        left join
+                        (select un.id, un.nickname, un.profile_img
+                        from user as un) as u
+                        on r.user_id = u.id;  ''',
+
+                        "recipe_ingredient" : '''select ifnull(ib.name, '재료')  as bundle ,i.name, ri.amount
+                                                from (select *
+                                                from recipe_ingredient
+                                                where recipe_id = %s) as ri
+                                                left join ingredient as i
+                                                on ri.ingredient_id = i.id
+                                                left join ingredient_bundle as ib
+                                                on ri.bundle_id = ib.id
+                                                order by bundle;''',
+
+                        "step" : '''select description, img
+                                    from recipe_step
+                                    where recipe_id = %s
+                                    order by step;'''
+
+
+}
--- resource/recipe.py
+++ resource/recipe.py
@@ -12,36 +12,38 @@ from config import Config
 from google.oauth2 import id_token as id_token_module
 from google.auth.transport import requests as google_requests
 
+from functions_for_recipe import recipe_detail_query
+
 
 class RescipeResource(Resource) :
     
     def get(self, recipe_id) : 
         # 요청한 레시피에 대한 상세정보 리턴하는 api
-        
+
         try :       
             # 1. db 접속
             connection = get_connection()
             cursor = connection.cursor(dictionary = True)
             # 2. 해당 테이블, recipe 테이블에서 select
-            query = recipe_list_map(list_type)
-            
-            cursor.execute(query)
+            query = recipe_detail_query("recipe_user_info")
+            record = (recipe_id, )
+            cursor.execute(query, record)
             # select 문은 아래 내용이 필요하다.
             # 커서로 부터 실행한 결과 전부를 받아와라.
             record_list = cursor.fetchall()
-            return_list = []
             i = 0
             for record in record_list:
-                # record_list[i]['created_at'] = record['created_at'].isoformat()
-                recipe = {"recipe_id": record['recipe_id'],
-                            "like": record['likes_cnt'],
-                            "view": record['views'],
-                            "userInfo":[record['profile_img'], record['nickname']],
+                recipe = {"recipe_id": record['id'],
+                            "title":record['title'],
+                            "mainSrc": record['mainSrc'],
+                            "intro": record['intro'],
+                            "writer":{ "id" : record['user_id'],"nickname," : record['nickname'], "profile_img" : record['profile_img']},
                             "public": record['public'],
-                            "src": record['header_img'],
-                            "title":record['header_title'],
-                            "created_at": record['created_at'].isoformat()}
-                return_list.append(recipe)
+                            "created_at": record['created_at'].isoformat(),     
+                            "updated_at": record['updated_at'].isoformat(),
+                            "category": [record['c_type'],record['c_ctx'],record['c_ind']],
+                            "recipeInfo":  [record['c_s'],record['c_time'],record['c_level']]}
+                print("i의 값은" + str(i))
                 i = i +1
 
         # 3. 클라이언트에 보낸다. 
@@ -49,6 +51,75 @@ class RescipeResource(Resource) :
             # 뒤의 e는 에러를 찍어라 error를 e로 저장했으니까!
             print('Error while connecting to MySQL', e)
             return {'status' : 500 ,'message' : str(e)}, HTTPStatus.INTERNAL_SERVER_ERROR
+        
+        
+        
+        try :
+            ingredients_list = []     
+            bundle = ""
+            contents_list =  []
+
+            # 2. 해당 테이블, recipe 테이블에서 select
+            query = recipe_detail_query("recipe_ingredient")
+            record = (recipe_id, )
+            cursor.execute(query, record)
+            # select 문은 아래 내용이 필요하다.
+            # 커서로 부터 실행한 결과 전부를 받아와라.
+            record_list = cursor.fetchall()
+            i = 0
+            for record in record_list:
+                if i == 0 :
+                    bundle = record["bundle"]
+                    contents_list.append([record["name"], record["amount"]])
+                    
+                else :
+                    if record["bundle"] == bundle:
+                        contents_list.append([record["name"], record["amount"]])
+
+                    else : 
+                        # bundle 변수의 값이  record["bundle"]  와 다르다면 ingredients_list 에 기존 값들을 저장해준 후 
+                        ingredients_list.append({ "name": bundle , "contents": contents_list})
+                        # bundle 재정의 후 재료 저장
+
+                        bundle = record["bundle"]
+                        contents_list.append([record["name"], record["amount"]])                
+                i = i +1
+            # 마지막으로 저장된 재료들도 리스트에 넣어주자.
+            ingredients_list.append({ "name": bundle , "contents": contents_list})
+            # 리턴할 딕셔너리에 넣어주자.
+            recipe["ingredients"] = ingredients_list
+
+        # 3. 클라이언트에 보낸다. 
+        except Error as e :
+            # 뒤의 e는 에러를 찍어라 error를 e로 저장했으니까!
+            print('Error while connecting to MySQL', e)
+            return {'status' : 500 ,'message' : str(e)}, HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+        try :
+            step_list = []
+
+            # 2. 해당 테이블, recipe 테이블에서 select
+            query = recipe_detail_query("step")
+            record = (recipe_id, )
+            cursor.execute(query, record)
+            # select 문은 아래 내용이 필요하다.
+            # 커서로 부터 실행한 결과 전부를 받아와라.
+            record_list = cursor.fetchall()
+            i = 0
+            for record in record_list:          
+                step_list.append([record["description"], record["img"]])
+                i = i +1
+
+            recipe["steps"] = step_list
+            
+        # 3. 클라이언트에 보낸다. 
+        except Error as e :
+            # 뒤의 e는 에러를 찍어라 error를 e로 저장했으니까!
+            print('Error while connecting to MySQL', e)
+            return {'status' : 500 ,'message' : str(e)}, HTTPStatus.INTERNAL_SERVER_ERROR       
+        
+
         # finally 는 try에서 에러가 나든 안나든, 무조건 실행하라는 뜻.
         finally : 
             print('finally')
@@ -58,4 +129,4 @@ class RescipeResource(Resource) :
                 print('MySQL connection is closed')
             else :
                 print('connection does not exist')
-        return {'status' : 200, 'list' : return_list }, HTTPStatus.OK
+        return {'status' : 200, 'recipeInfo' : recipe }, HTTPStatus.OK
